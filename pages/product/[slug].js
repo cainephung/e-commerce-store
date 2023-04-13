3e093033e734f1b0af74d91ffa7ab7eaaa90e661
cainephung <106492108+cainephung@users.noreply.github.com>
2023-04-13 02:07:12 -0400

    TypeError: Cannot read properties of null (reading 'useState')

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,21 +3,20 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
-// import { useStateContext } from '../../context/StateContext';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-//   const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
-//   const handleBuyNow = () => {
-//     onAdd(product, qty);
+  const handleBuyNow = () => {
+    onAdd(product, qty);
 
-//     setShowCart(true);
-//   }
+    setShowCart(true);
+  }
 
   return (
-   
     <div>
       <div className="product-detail-container">
         <div>
@@ -56,24 +55,14 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              {/* <span className="minus" onClick={decQty}><AiOutlineMinus /></span> */}
-              <span className="minus" onClick=""><AiOutlineMinus /></span>
-
-              {/* <span className="num">{qty}</span> */}
-              <span className="num">qty</span>
-
-              {/* <span className="plus" onClick={incQty}><AiOutlinePlus /></span> */}
-              <span className="plus" onClick=""><AiOutlinePlus /></span>
-
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className="buttons">
-            {/* <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button> */}
-            <button type="button" className="add-to-cart" onClick="">Add to Cart</button>
-
-            {/* <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button> */}
-            <button type="button" className="buy-now" onClick="">Buy Now</button>
-
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
+            <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
           </div>
         </div>
       </div>
@@ -121,7 +110,7 @@ export const getStaticProps = async ({ params: { slug }}) => {
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
 
-  console.log(product);
+//   console.log(product);
 
   return {
     props: { products, product }
